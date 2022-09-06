4d548109f6fd88579754680aa1f5b38bc72f0824
DESKTOP-6FBGBC6\GDJEva <gdjevans@hotmail.com>
2022-09-06 04:05:37 +0100

    Syntax start

--- ConsoleWinForms/ConsoleProgramming/HelloWorld/Program.cs
+++ ConsoleWinForms/ConsoleProgramming/HelloWorld/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+/*
+    There are three control structures in C# Programming; Serquence, Decisions and Repetition.
+ */
+
+
+//This line prints hello world.
 Console.WriteLine("Hello, World!");
