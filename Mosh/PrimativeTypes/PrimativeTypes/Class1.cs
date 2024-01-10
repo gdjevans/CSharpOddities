be0997eb963fc8067cc606ad218fb8d77159d63e
DESKTOP-0QATB9U\GDJEvans <gdjevans@hotmail.com>
2024-01-10 01:00:31 +0000

    Variables

---
+++ Mosh/PrimativeTypes/PrimativeTypes/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrimativeTypes
+{
+    public class Class1
+    {
+        int number;
+
+        int Number = 1;
+
+        const float Pi = 3.14f;
+    }
+}
