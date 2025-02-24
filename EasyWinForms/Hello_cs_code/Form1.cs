57513beb65195e2408e37fb2a54706b6c495798e
SUMMER_MASH\pinin <evansgdj@hotmail.com>
2025-02-24 23:37:08 +0000

    Variables - Create a clock

--- EasyWinForms/Hello_cs_code/Form1.cs
+++ EasyWinForms/Hello_cs_code/Form1.cs
@@ -37,9 +37,52 @@ namespace Hello_cs_code
 
         private void button5_Click(object sender, EventArgs e)
         {
-            this.textBox1.Text = "Hello I'm a berautiful Textbox"; 
+            this.textBox1.Text = "Hello I'm a berautiful Textbox";
             this.button3.Text = "Hi Babe!";
             this.Text = "Hi, I love you; Me too!!!!!!";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //this.textBox2.Text = DateTime.Now.ToString();
+            //this.textBox2.Text = DateTime.Now.DayOfWeek.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.panel1.Enabled = true;
+        }
+
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.panel1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.textBox2.Text = DateTime.Now.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //this.textBox3.Text = DateTime.Now.ToString();
+            this.textBox3.Text = DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString() + ":" +  DateTime.Now.Second.ToString() + ":" + DateTime.Now.Millisecond.ToString();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            this.timer2.Enabled = false;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            this.timer2.Enabled = true;
         }
     }
 }
