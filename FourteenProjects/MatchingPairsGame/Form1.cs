4c9099c7123e90750c96edd51f6e219a6ce56c05
SUMMER_MASH\pinin <evansgdj@hotmail.com>
2025-03-25 22:13:12 +0000

    Finish Matching Pairs Game

--- FourteenProjects/MatchingPairsGame/Form1.cs
+++ FourteenProjects/MatchingPairsGame/Form1.cs
@@ -35,6 +35,10 @@ namespace MatchingPairsGame
 
         private void label_click(object sender, EventArgs e)
         {
+            if (timer1.Enabled == true)
+            {
+                return;
+            }
             Label clickedLabel = sender as Label;
             if (clickedLabel != null)
             {
@@ -42,7 +46,7 @@ namespace MatchingPairsGame
                     return;
 
                 //clickedLabel.ForeColor = Color.Black;
-                if(firstClicked == null)
+                if (firstClicked == null)
                 {
                     firstClicked = clickedLabel;
                     firstClicked.ForeColor = Color.Black;
@@ -50,7 +54,45 @@ namespace MatchingPairsGame
                     return;
 
                 }
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                CheckForWinner();
+
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+
+                timer1.Start();
             }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+        private void CheckForWinner()
+        {
+            foreach(Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label; 
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+
+            MessageBox.Show("All icons have been matched. Congratulations and well done! ");
+            Close();
         }
     }
 }
