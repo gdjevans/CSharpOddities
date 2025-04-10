34e8e054e0931e99c86d59678b16deb93752c5c8
SUMMER_MASH\pinin <evansgdj@hotmail.com>
2025-04-10 21:03:22 +0100

    Start Web Browser project.

---
+++ FourteenProjects/WebBrowser/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            toolStrip1 = new ToolStrip();
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            btnBack = new ToolStripButton();
+            btnForward = new ToolStripButton();
+            toolStripSeparator1 = new ToolStripSeparator();
+            toolStripComboBox1 = new ToolStripComboBox();
+            btnGo = new ToolStripButton();
+            toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Items.AddRange(new ToolStripItem[] { btnBack, btnForward, toolStripSeparator1, toolStripComboBox1, btnGo });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(800, 25);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = Color.White;
+            webView21.Location = new Point(0, 28);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(800, 420);
+            webView21.TabIndex = 1;
+            webView21.ZoomFactor = 1D;
+            // 
+            // btnBack
+            // 
+            btnBack.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnBack.Image = (Image)resources.GetObject("btnBack.Image");
+            btnBack.ImageTransparentColor = Color.Magenta;
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(36, 22);
+            btnBack.Tag = "";
+            btnBack.Text = "Back";
+            // 
+            // btnForward
+            // 
+            btnForward.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnForward.Image = (Image)resources.GetObject("btnForward.Image");
+            btnForward.ImageTransparentColor = Color.Magenta;
+            btnForward.Name = "btnForward";
+            btnForward.Size = new Size(54, 22);
+            btnForward.Text = "Forward";
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(6, 25);
+            // 
+            // toolStripComboBox1
+            // 
+            toolStripComboBox1.Name = "toolStripComboBox1";
+            toolStripComboBox1.Size = new Size(121, 25);
+            // 
+            // btnGo
+            // 
+            btnGo.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            btnGo.Image = (Image)resources.GetObject("btnGo.Image");
+            btnGo.ImageTransparentColor = Color.Magenta;
+            btnGo.Name = "btnGo";
+            btnGo.Size = new Size(28, 22);
+            btnGo.Text = "GO";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(webView21);
+            Controls.Add(toolStrip1);
+            Name = "Form1";
+            Text = "Web Browser";
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private ToolStripButton btnBack;
+        private ToolStripButton btnForward;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripComboBox toolStripComboBox1;
+        private ToolStripButton btnGo;
+    }
+}
