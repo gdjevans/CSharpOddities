63fed8a3ff10d956f030dcbb82ce87597249e217
SUMMER_MASH\pinin <evansgdj@hotmail.com>
2025-04-14 16:46:23 +0100

    Windows Of Visual Studio

---
+++ OakProjects/NecessaryWindows/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace NecessaryWindows
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button3 = new Button();
+            panel3 = new Panel();
+            button2 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton5 = new RadioButton();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            radioButton6 = new RadioButton();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(57, 286);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 41);
+            button1.TabIndex = 0;
+            button1.Text = "Character";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(comboBox2);
+            panel1.Controls.Add(radioButton6);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(radioButton3);
+            panel1.Controls.Add(radioButton4);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(23, 40);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(234, 383);
+            panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button3);
+            panel2.Location = new Point(540, 40);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(234, 383);
+            panel2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(57, 286);
+            button3.Name = "button3";
+            button3.Size = new Size(120, 41);
+            button3.TabIndex = 0;
+            button3.Text = "UpKeep";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(comboBox1);
+            panel3.Controls.Add(radioButton5);
+            panel3.Controls.Add(textBox1);
+            panel3.Controls.Add(radioButton2);
+            panel3.Controls.Add(radioButton1);
+            panel3.Controls.Add(button2);
+            panel3.Location = new Point(283, 40);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(234, 383);
+            panel3.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(57, 286);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 41);
+            button2.TabIndex = 0;
+            button2.Text = "Story";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(43, 43);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(80, 19);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Display All";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(42, 80);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(48, 19);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Find";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(42, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(159, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(34, 105);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(159, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(34, 80);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(48, 19);
+            radioButton3.TabIndex = 5;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Find";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(35, 43);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(80, 19);
+            radioButton4.TabIndex = 4;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Display All";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(42, 144);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(119, 19);
+            radioButton5.TabIndex = 4;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "Display Selected...";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(42, 173);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(159, 23);
+            comboBox1.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(34, 173);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(159, 23);
+            comboBox2.TabIndex = 8;
+            // 
+            // radioButton6
+            // 
+            radioButton6.AutoSize = true;
+            radioButton6.Location = new Point(34, 144);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(119, 19);
+            radioButton6.TabIndex = 7;
+            radioButton6.TabStop = true;
+            radioButton6.Text = "Display Selected...";
+            radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "frmMain";
+            Text = "Main";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Panel panel1;
+        private Panel panel2;
+        private Button button3;
+        private Panel panel3;
+        private RadioButton radioButton1;
+        private Button button2;
+        private TextBox textBox2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private ComboBox comboBox1;
+        private RadioButton radioButton5;
+        private TextBox textBox1;
+        private RadioButton radioButton2;
+        private ComboBox comboBox2;
+        private RadioButton radioButton6;
+    }
+}
---
+++ OakProjects/NecessaryWindows/Form1.cs
@@ -0,0 +1,20 @@
+namespace NecessaryWindows
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
