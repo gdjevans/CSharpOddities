76fafcab4868aae9d69461704b000fe1c6ba8c05
SUMMER_MASH\pinin <evansgdj@hotmail.com>
2025-07-19 19:31:03 +0100

    Application Paths

---
+++ EasyWinForms/Debugging/SizingProgram/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SizingProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //this.Size = new Size(950, 489);
+
+            Int32 h;
+            h = this.Size.Height;
+            this.Size = new Size(950, h);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Int32 h;
+            h = this.Size.Height;
+            this.Size = new Size(816, h);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Size = new Size(this.pictureBox1.Width + 10, this.pictureBox1.Height + 10);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Size = new Size(this.pictureBox1.Width - 10, this.pictureBox1.Height - 10);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Location = new Point(this.pictureBox1.Location.X + 10, this.pictureBox1.Location.Y);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Location = new Point(this.pictureBox1.Location.X - 10, this.pictureBox1.Location.Y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = e.X.ToString() + ":" + e.Y.ToString();
+            this.pictureBox1.Location = new Point(e.X, e.Y);
+        }
+    }
+}
