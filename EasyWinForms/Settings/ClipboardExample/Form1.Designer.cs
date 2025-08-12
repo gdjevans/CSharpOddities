06606358d349a1fe1d3200d78b49ca05ea85f60d
SUMMER_MASH\pinin <evansgdj@hotmail.com>
2025-08-12 16:49:14 +0100

    Getting Application/Version Info

--- EasyWinForms/Settings/ClipboardExample/Form1.Designer.cs
+++ EasyWinForms/Settings/ClipboardExample/Form1.Designer.cs
@@ -28,13 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,7 +49,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(285, 74);
+            this.button1.Location = new System.Drawing.Point(287, 60);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(164, 32);
             this.button1.TabIndex = 1;
@@ -58,7 +59,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(285, 177);
+            this.button2.Location = new System.Drawing.Point(287, 135);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(164, 32);
             this.button2.TabIndex = 3;
@@ -68,7 +69,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(209, 133);
+            this.textBox2.Location = new System.Drawing.Point(211, 102);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(326, 20);
             this.textBox2.TabIndex = 2;
@@ -76,9 +77,9 @@
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(270, 230);
+            this.pictureBox1.Location = new System.Drawing.Point(272, 188);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(195, 199);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -87,7 +88,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(285, 447);
+            this.button3.Location = new System.Drawing.Point(287, 405);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(164, 32);
             this.button3.TabIndex = 5;
@@ -95,11 +96,33 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(287, 443);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(164, 32);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Read Image from Clipboard";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(287, 481);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(164, 32);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Clear Clipboard";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 573);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
@@ -122,6 +145,8 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
---
+++ EasyWinForms/Settings/EnvironDetails/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EnvironDetails")]
+[assembly: AssemblyDescription("Banished Details")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TwistedFreak")]
+[assembly: AssemblyProduct("EnvironDetails")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("TF")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("575df032-c572-414b-a012-ab21528b89db")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.1.11.42")]
+[assembly: AssemblyFileVersion("1.1.32.8")]
