8ad0fe92b2eb77009e2870aca55b559a248e24d9
SUMMER_MASH\pinin <evansgdj@hotmail.com>
2025-10-03 23:36:06 +0100

    Other Operators

---
+++ CompMasterClass/Section 04/ConditionalLogic/AssignmentOperator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalLogic
+{
+    internal class AssignmentOperator
+    {
+        static void Main(string[] args)
+        {
+            int c = 10;
+            c += 5;
+            Console.WriteLine(c);
+
+            int x = 10, y = 10;
+            if (x == y)
+            {
+                Console.WriteLine("Both Are Equal");
+            }
+
+            bool z = true;
+            if (z = false)
+            {
+                Console.WriteLine("This will not be printed to the console.");
+            }
+            Console.WriteLine("The value of 'z' after the if statement: " + z.ToString());
+        }
+    }
+}
