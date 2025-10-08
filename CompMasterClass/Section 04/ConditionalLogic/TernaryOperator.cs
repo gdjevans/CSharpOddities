a1689c0ff542abc2532aa3227136da40d738844a
SUMMER_MASH\pinin <evansgdj@hotmail.com>
2025-10-08 21:11:10 +0100

    Ternary Operators

---
+++ CompMasterClass/Section 04/ConditionalLogic/TernaryOperator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalLogic
+{
+    internal class TernaryOperator
+    {
+        static void Main(string[] args)
+        {
+            int age = 20;
+            String status;
+            status = (age < 18) ? "Under age" : "Senior";
+            Console.WriteLine(status);
+
+            string nationality = "French";
+            string isFrench;
+            isFrench = (nationality == "French") ? "True" : "False";
+            Console.WriteLine(isFrench);
+            int years = 30;
+            string userStatus;
+            userStatus = (years < 18) ? "Minor" : "Adult";
+            Console.WriteLine(userStatus);
+        }
+    }
+}
