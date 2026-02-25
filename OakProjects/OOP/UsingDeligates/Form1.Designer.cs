e99b2b328e2e95721e6db4fdf33d6b05275aab6e
SUMMER_MASH\pinin <evansgdj@hotmail.com>
2026-02-25 22:06:06 +0000

    Using Delegates

---
+++ OakProjects/OOP/UsingDeligates/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace UsingDeligates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(571, 52);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(108, 51);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Show";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(129, 54);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(363, 328);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ OakProjects/OOP/UsingDeligates/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsingDeligates
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public delegate void fillTextBox(int x);
+        string textvalue;
+        public void FillMathNote(int ExamNote)
+        {
+            textvalue += "Your Math Note is: " + ExamNote + Environment.NewLine;
+        }
+        public void FillChemistryNote(int ExamNote)
+        {
+            textvalue += "Your Chemistry Note is: " + ExamNote + Environment.NewLine;
+        }
+        public void ShowInTextBox()
+        {
+            textBox1.Text = textvalue;
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            fillTextBox filltext = new fillTextBox(FillMathNote);
+            filltext += FillChemistryNote;
+            filltext(80);
+            filltext -= FillMathNote;
+            filltext(60);
+
+            ShowInTextBox();
+        }
+    }
+}
