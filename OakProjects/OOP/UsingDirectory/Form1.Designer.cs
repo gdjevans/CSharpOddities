da63c25160410470c2ebefec650d8dea4ffe50a9
SUMMER_MASH\pinin <evansgdj@hotmail.com>
2026-02-27 17:37:13 +0000

    Directory Classes

---
+++ OakProjects/OOP/UsingDirectory/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace UsingDirectory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.Location = new System.Drawing.Point(55, 51);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(119, 46);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnMove
+            // 
+            this.btnMove.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMove.Location = new System.Drawing.Point(220, 51);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(119, 46);
+            this.btnMove.TabIndex = 1;
+            this.btnMove.Text = "Move";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(388, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnMove);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ OakProjects/OOP/UsingDirectory/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace UsingDirectory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory("temp");
+            DirectoryInfo dr = new DirectoryInfo("temp2");
+            dr.Create();
+            MessageBox.Show("Folder was created");
+
+            DirectoryInfo drr = new DirectoryInfo("folder2");
+            string accessTime = drr.LastAccessTime.ToString();
+            MessageBox.Show(accessTime);
+        }
+
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            Directory.Move("temp", "new");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Directory.Delete("temp");
+            MessageBox.Show("Folder deleted");
+        }
+    }
+}
