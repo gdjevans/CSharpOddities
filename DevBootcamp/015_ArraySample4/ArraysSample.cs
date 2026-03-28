df0f594983635100641c06360498f97a12710a79
SUMMER_MASH\pinin <evansgdj@hotmail.com>
2026-03-28 21:31:29 +0000

    Finishing Array Section

---
+++ DevBootcamp/015_ArraySample4/ArraysSample.cs
@@ -0,0 +1,15 @@
+using System; 
+class Program 
+{ 
+	static void Main() 
+	{ 
+		// Sample - Arrays 4
+		int[] a = new[] { 1, 10, 100, 1000 }; // int[]
+
+		// Accessing array
+		Console.WriteLine("First element: " + a[0]);
+		Console.WriteLine("Second element: " + a[1]);
+		Console.WriteLine("Third element: " + a[2]);
+		Console.WriteLine("Fourth element: " + a[3]);
+	} 
+}
