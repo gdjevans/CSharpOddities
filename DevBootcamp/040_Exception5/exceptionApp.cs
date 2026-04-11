c15d52c78c405b45b76e39de3b4efea796409e83
SUMMER_MASH\pinin <evansgdj@hotmail.com>
2026-04-11 18:54:58 +0100

    Exception Handling with Serilog

---
+++ DevBootcamp/040_Exception5/exceptionApp.cs
@@ -0,0 +1,20 @@
+using System; 
+using Serilog;
+
+class Program
+{
+    static void Main()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()	
+            .CreateLogger();
+
+        Log.Information("Hello, Serilog!");
+        Log.Warning("This is a warning message.");
+        Log.Error("An error occurred.");
+
+        Log.CloseAndFlush();
+    }
+}
+
